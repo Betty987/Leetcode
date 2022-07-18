82d3600a178dfb8e57cdf660bfd1a4ebc39d54b6
Betty <93065948+Betty987@users.noreply.github.com>
2022-07-18 21:21:12 +0800

    Time: 1 ms (92.53%), Space: 41.6 MB (77.75%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -1,9 +1,9 @@
 class Solution {
     public String sortSentence(String s) {
-     HashMap<Integer,String> map = new HashMap<>();
-        for(String word:s.split(" ")){
+     HashMap<Integer,String> map = new HashMap<>();//hashmap
+        for(String word:s.split(" ")){//for every word split by space
        
-         int index= word.charAt(word.length()-1);
+         int index= word.charAt(word.length()-1);//last index of the word
          String actualword= word.substring(0,word.length()-1);
          map.put(index,actualword);
         }
@@ -13,6 +13,6 @@ class Solution {
             actualstring.append(ind.getValue());
             actualstring.append(" ");
         }
-        return actualstring.toString().trim();
+        return actualstring.toString().trim();//a string builder has to be converted to string
     }
 }
