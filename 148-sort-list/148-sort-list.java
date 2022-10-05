63a3f3fb312e02b7d6438a622a1424b4556efaa6
Bethlehem <93065948+Betty987@users.noreply.github.com>
2022-10-05 14:13:40 +0800

    Time: 18 ms (63.31%), Space: 80 MB (25.58%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -10,51 +10,53 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if(head==null||head.next==null) return head;
-        //split in half
-        ListNode l1=head;
-        ListNode l2=splitmidnode(head);
-        //sort
-        l1=sortList(l1);
-        l2=sortList(l2);
+        if(head==null || head.next==null)
+            return head;
         
-        ListNode dummy=new ListNode(),res=dummy;
+        ListNode l1 = head,l2=splitmidnode(head);
+        
+        l1=sortList(l1);
+        l2 = sortList(l2);
+        
+        ListNode dummy = new ListNode(),res=dummy;
         
         while(l1!=null&&l2!=null){
-            if(l1.val<=l2.val){
-                res.next=l1;
+            if(l1.val < l2.val){
+                res.next = l1;
                 l1=l1.next;
             }
             else{
-                res.next=l2;
+                res.next = l2;
                 l2=l2.next;
             }
-            res=res.next;  
+         res=res.next;
         }
-        if(l2!=null){
-            res.next=l2;
-           
-        }
-        else{
+        if(l1!=null){//but l2 is null
             res.next=l1;
-          
+        }
+        else{//l1 is null
+            res.next=l2;
         }
         
-       return dummy.next; 
+       return dummy.next;
+      
     }
-    
     
     private ListNode splitmidnode(ListNode head){
+        ListNode temp=null;
         ListNode slow=head;
         ListNode fast=head;
-        ListNode temp=head;
-        
-        while(fast!=null&&fast.next!=null){
-             temp=slow;
+        while(fast!=null && fast.next!=null){
+            temp=slow;
+            fast=fast.next.next;
             slow=slow.next;
-            fast=fast.next.next;  
+            
         }
-        temp.next=null;
-        return slow;
+        temp.next=null;//temp is the end of the left half
+        return slow;//slow is the head of the right half
+        
     }
-}
+    
+    
+
+    }
