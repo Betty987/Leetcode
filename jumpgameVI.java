d939a704dfff6dcf5b7654ded554162b81558129
Bethlehem <93065948+Betty987@users.noreply.github.com>
2023-02-13 19:08:49 +0800

    Create jumpgameVI.java

---
+++ jumpgameVI.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(e->-e[0]));
+        for(int i = 0; i < n; i++) {
+            while(q.size() > 0 && q.peek()[1] + k < i) {
+                q.poll();
+            }
+            dp[i] = nums[i] + (q.size() > 0 ? q.peek()[0] : 0);
+            q.add(new int[]{dp[i], i});
+        }
+        return dp[n - 1];
+    }
+}
