92f58124742bc0eb6492d3182431b852edb56336
Bethlehem <93065948+Betty987@users.noreply.github.com>
2023-02-13 13:32:14 +0800

    Create find the kth largest integer in the array.java

---
+++ find the kth largest integer in the array.java
@@ -0,0 +1,30 @@
+import java.math.BigInteger;
+class Solution {
+  
+    
+      public String kthLargestNumber(String[] nums, int k) {
+                
+        int N = nums.length;
+        
+        PriorityQueue<BigInteger> pq = new PriorityQueue();
+        
+          
+        for(int i=0; i < N ; i++) {
+            pq.offer(new BigInteger(nums[i]));
+        }
+               
+        BigInteger number=null;
+        int limit=(N-k+1);
+        
+        int i=0;
+          
+        while(i< limit) {
+            number = pq.poll();  
+            i++;
+        }
+        
+        return number.toString();
+        
+    }
+    
+}
