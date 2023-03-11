673d2a1c81669acbbcc148b07afa3040b7672c67
Bethlehem <93065948+Betty987@users.noreply.github.com>
2023-03-12 01:32:48 +0800

    Create evaluatereversepolishnotation.java

---
+++ evaluatereversepolishnotation.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack();
+        for(int i=0;i<tokens.length;i++){
+            switch(tokens[i]){
+                case "+":
+                    stack.push(stack.pop()+stack.pop());
+                    break;
+                case "*":
+                    stack.push(stack.pop()*stack.pop());
+                    break;
+                case "-":
+                    int val1=stack.pop();
+                    int val2=stack.pop();
+                    stack.push(val2-val1);
+                    break;
+                case "/":
+                    val1=stack.pop();
+                    val2=stack.pop();
+                    stack.push(val2/val1);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        return stack.pop();
+    }
+}
