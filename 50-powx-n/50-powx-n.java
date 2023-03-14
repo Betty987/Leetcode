f63d3f08c273f8d5978090813f76d02eda6b6bfd
Bethlehem <93065948+Betty987@users.noreply.github.com>
2023-03-14 12:40:16 +0800

    Update 50-powx-n.java

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,24 +1,11 @@
 class Solution {
-    public double myPow(double x, int n) {
-       double ans=1;
-       long exp=n;
-        if(exp<0) exp=-1*exp;
-        while(exp>0){
-            if(exp%2==1){
-                ans=ans*x;
-                exp=exp-1;
-            }
-            else{
-                x=x*x;
-                exp=exp/2;
-            }
-        }
-        if(n<0) ans =(double)1/(double)ans;
-        return ans;
-        
-    
-             
-        }
-        
-        
-    }
+  public double myPow(double x, long n) {
+    if (n == 0)
+      return 1;
+    if (n < 0)
+      return 1 / myPow(x, -n);
+    if (n % 2 == 1)
+      return x * myPow(x, n - 1);
+    return myPow(x * x, n / 2);
+  }
+}
