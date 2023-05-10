de498c2e0d318bb6c33731157fb4160307744781
Bethlehem <93065948+Betty987@users.noreply.github.com>
2023-05-10 11:31:26 +0800

    Update 0204-count-primes.java

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,25 +1,16 @@
 class Solution {
     public int countPrimes(int n) {
-        
-        //space 
-        int count=0;
-        //sieve of eratosthenes
-       boolean primes[] = new boolean[n+1];//n+1 to include 0
-       for(int i=0;i<primes.length;i++){
-           primes[i]=true;      //we set everything to true
-       }
-        
-      for(int i=2;i*i<=n;i++){
-          if(primes[i]){
-              for(int j=i*i;j<=n;j+=i){
-                  primes[j]=false;
-              }
-          }
-      }
-      for(int i=2;i<n;i++){
-          if(primes[i]) count++;
-      }
-        return count;
+        boolean[] isprime = new boolean[n];
+        Arrays.fill(isprime,true);//we set everything to true
+        int primecount=0;
+        for(int i=2;i<isprime.length;i++){
+            if(isprime[i]){//if it is prime we count it
+                primecount++;
+                for(int j=i+i;j<isprime.length;j+=i){//we set the multiples to false
+                        isprime[j]=false;
+                }
+            }
+        }
+        return primecount;
     }
-  
 }
