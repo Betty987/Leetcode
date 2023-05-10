3cb24fee5a1d568ed976029344a2e69a1c52c508
Bethlehem <93065948+Betty987@users.noreply.github.com>
2023-05-10 10:09:55 +0800

    Update 461-hamming-distance.java

--- 461-hamming-distance/461-hamming-distance.java
+++ 461-hamming-distance/461-hamming-distance.java
@@ -1,11 +1,7 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int result=0;
-        while(x>0||y>0){ 
-        result+=(x%2)^(y%2);//this gets the last digit in binary and xor is 1 if they are different
-        x>>=1;
-        y>>=1;
-        }
-        return result;
+
+        //integer.bitCount gets the number of 1s in the binary representation of a number
+        return Integer.bitCount(x^y);
     }
 }
