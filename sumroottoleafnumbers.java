be5f5b450af1eb83996a64cf64d113e3e4ae2057
Bethlehem <93065948+Betty987@users.noreply.github.com>
2023-05-26 08:53:13 +0800

    Create sumroottoleafnumbers.java

---
+++ sumroottoleafnumbers.java
@@ -0,0 +1,36 @@
+class Solution {
+    static ArrayList<String> sum( TreeNode root , ArrayList<String> arr , String str )
+    {
+        if( root==null ) return arr;
+        if( root.left ==null && root.right==null ){
+            str+=Integer.toString(root.val) ;
+            arr.add( str );
+        }
+        if( root.left!=null && root.right != null ){
+            str+=Integer.toString(root.val) ;
+            sum( root.left , arr , str );
+            sum( root.right , arr , str );
+        }
+        if( root.left!=null && root.right == null ){
+            str+=Integer.toString(root.val) ;
+            sum( root.left , arr , str  );
+        }
+        if( root.left==null && root.right != null ) {
+            str+=Integer.toString(root.val) ;
+            sum( root.right , arr , str  );
+        }
+
+        return arr ;
+
+    }
+    public int sumNumbers(TreeNode root) {
+        ArrayList<String> arr = new ArrayList<>();
+        int sum = 0;
+        sum( root , arr , ""  );
+        for( int i = 0 ; i<arr.size() ; i++ )
+        {
+            sum+= Integer.valueOf( arr.get(i) );
+        }
+        return sum  ;
+    }
+}
